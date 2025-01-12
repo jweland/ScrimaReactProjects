b286d5b0d895ab515d11eac8123193b22c4f8e45
jweland <jillianweland@outlook.com>
2025-01-12 16:14:18 -0600

    Custom Components

--- ReactFacts/react-facts/src/index.jsx
+++ ReactFacts/react-facts/src/index.jsx
@@ -1,23 +1,30 @@
 import { createRoot } from "react-dom/client";
-import { Fragment } from "react";
 
 const root = createRoot(document.getElementById("root"))
 root.render(
     <TempName />
 )
 
+function Header() {
+    return (
+        <header>
+            <h1 className="h1">Fun Facts About React!</h1>
+        </header>
+    )
+}
+
 function TempName(){
     return (
-    <main className="body">
-        <img src="src\react-logo.jpg" width="40" alt="React Logo"/>
-        <h1 className="h1">Fun Facts About React!</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on Github</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </main>
-)
+        <>
+            <img src="src\react-logo.jpg" width="40" alt="React Logo"/>
+            <Header />
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </>
+    )
 }
