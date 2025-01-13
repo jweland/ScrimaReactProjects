ee6cd4116bbfd1c71d88ace7736b09249f6511e3
jweland <jillianweland@outlook.com>
2025-01-13 17:31:11 -0600

    Creating TravelerJournal

---
+++ TravelJournal/travel-journal/src/Components/Entry.jsx
@@ -0,0 +1,10 @@
+export default function Entry(){
+    return (
+        <article>
+            <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount Fuji" />
+            <div>
+                <img />
+            </div>
+        </article>
+    )
+}
---
+++ TravelJournal/travel-journal/src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="./src/globe.png" alt="Globe icon" className="globe"></img>
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
