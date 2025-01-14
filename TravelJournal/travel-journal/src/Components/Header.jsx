0d2acae7be14d78426b328cb96fddbff733e1867
jweland <jillianweland@outlook.com>
2025-01-14 10:10:04 -0600

    Adding Props

--- TravelJournal/travel-journal/src/Components/Header.jsx
+++ TravelJournal/travel-journal/src/Components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <img src="./src/globe.png" alt="Globe icon" className="globe"></img>
+            <img src="./src/images/globe.png" alt="Globe icon" className="globe"></img>
             <h1>my travel journal.</h1>
         </header>
     )
