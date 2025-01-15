4f32077ed94991967aaa719d9695db125974d7b8
jweland <jillianweland@outlook.com>
2025-01-15 11:16:16 -0600

    Mapping Data

--- TravelJournal/travel-journal/src/App.jsx
+++ TravelJournal/travel-journal/src/App.jsx
@@ -1,12 +1,25 @@
 import Header from "./Components/Header"
 import Entry from "./Components/Entry"
+import data from "./data"
 
 export default function App() {
+    const entries = data.map(entry => 
+    {
+        return <Entry 
+        img={{src: entry.img.src, alt: entry.img.alt}}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+        />
+    })
+
     return (
         <>
             <Header />
             <main className="container">
-                <Entry />
+                {entries}
             </main>
         </>
     )
