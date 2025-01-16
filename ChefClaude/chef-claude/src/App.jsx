dae6afe09ed4ae019f13b2fde5417ea6c6187281
jweland <jillianweland@outlook.com>
2025-01-16 11:21:19 -0600

    ChefClaude Static Elements

---
+++ ChefClaude/chef-claude/src/App.jsx
@@ -0,0 +1,16 @@
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
+import Header from "./Components/Header"
+import Main from "./Components/Main"
+
+export default function App() {
+    return (
+        <>
+            <Header/>
+            <Main/>
+        </>
+    )
+  }
+  
---
+++ ChefClaude/chef-claude/src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="src/chef-claude.jpg" alt="Whisk and Roller" className="chef-claude"/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
