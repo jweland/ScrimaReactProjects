533ffae49ff8b3557dd06c3cb5be3764332c731f
jweland <jillianweland@outlook.com>
2025-01-23 09:41:18 -0600

    Edit form and update dependencies

--- ChefClaude/chef-claude/src/App.jsx
+++ ChefClaude/chef-claude/src/App.jsx
@@ -1,7 +1,3 @@
-/**
- * Challenge: Build the Header component in a separate file
- * and render it here in the App component
- */
 import Header from "./Components/Header"
 import Main from "./Components/Main"
 
