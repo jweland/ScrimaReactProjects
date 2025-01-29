1a66d1ef7a694af7698b7099df7a42950e53af58
jweland <jillianweland@outlook.com>
2025-01-29 13:05:31 -0600

    toggle update

--- SoundPads/src/App.jsx
+++ SoundPads/src/App.jsx
@@ -3,21 +3,16 @@ import React from "react"
 import Pad from "./Pad.jsx"
 
 export default function App(props) {
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from pads.js
-     * 2. Map over that state array and display each one
-     *    as a <button> (CSS is already written for you)
-     *    (Don't worry about using the "on" or "color" 
-     *    properties yet)
-     */
     const [pads, setPad] = React.useState(padsData)
 
     const darkMode = props.darkMode ? "#222222" : "#cccccc"
 
+    function toggle() {
+        console.log(Clicked)
+    }
+
     const buttonElement = pads.map(pad => (
-      <Pad key={pad.id} color={pad.color} on={pad.on}/>
+      <Pad toggle={toggle} key={pad.id} color={pad.color} on={pad.on}/>
     ))
 
     return (
